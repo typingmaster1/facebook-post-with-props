43fea85e56ab8b7f2e00a88087fd401055e38cc3
typingmaster1 <typingmaster500@gmail.com>
2024-05-29 23:48:38 -0700

    added

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import FacebookPost from './FacebookPost'
+import './App.css'
+import profile from "./assets/profile.png"
+import post from "./assets/post.png"
+import post2 from "./assets/post2.png"
+import wicket from "./assets/wicket.png"
+function App() {
+  
+
+  return (
+    <>
+ <div className="App">
+      <FacebookPost
+        profilePic={profile}
+        author="Zaroon Bahadur"
+        time="14 hours ago"
+        post={post}
+      />
+      <FacebookPost
+       profilePic={profile}
+        author="Z Ur Rehman"
+        time="2 hours ago"
+        post={post2}
+      />
+      <FacebookPost
+       profilePic={profile}
+        author="Zia Ur Rehman"
+        time="2 hours ago"
+        post={wicket}
+      />
+      
+    </div>
+    </>
+  )
+}
+
+export default App
